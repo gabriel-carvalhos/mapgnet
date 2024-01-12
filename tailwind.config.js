cc5df2180b5c26065d7ae848e8e07f71b1891cfb
gabrioviski <gabrielcarvalhos.snt@gmail.com>
2024-01-12 01:19:42 -0300

    add tailwindcss/form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./js/index.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
 
