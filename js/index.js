04a7415ef634d0ecbb54cdbcea28d8913420d683
gabrioviski <gabrielcarvalhos.snt@gmail.com>
2024-01-18 19:19:48 -0300

    add marcadores

--- js/index.js
+++ js/index.js
@@ -2,24 +2,43 @@ const accessToken = 'pk.eyJ1IjoiZ2FicmllbGNhcnZhbGgwIiwiYSI6ImNscjltcjF2ZjAzZW4y
 const input = document.querySelector('.input')
 const suggestions = document.querySelector('.suggestions')
 
-navigator.geolocation.getCurrentPosition(
-    ({ coords: {latitude, longitude} }) => setupMap(longitude, latitude),
-    () => setupMap(-46.625290, -23.533773),
-    { enableHighAccuracy: true }
-)
-
-const setupMap = (lng, lat) => {
-    mapboxgl.accessToken = accessToken
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/navigation-night-v1',
-        center: [lng, lat],
-        zoom: 12,
-        minZoom: 5,
-        language: 'pt'
+const getUserPosition = () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy: true })
     })
-    map.addControl(new mapboxgl.NavigationControl())
 }
+
+const setUserCoords = async () => {
+    try {
+        const { coords: {latitude, longitude} } = await getUserPosition()
+        return [longitude, latitude]
+    } catch (err) {
+        return [-46.625290, -23.533773]
+    }
+}
+
+const coords = await setUserCoords()
+
+mapboxgl.accessToken = accessToken
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v11',
+    center: coords,
+    zoom: 12,
+    minZoom: 5,
+    language: 'pt'
+})
+map.addControl(new mapboxgl.NavigationControl())
+
+const start = new mapboxgl.Marker({
+    color: '#2C6493',
+    draggable: true
+}).setLngLat(coords)
+
+const end = new mapboxgl.Marker({
+    color: '#ef9652',
+    draggable: true
+})
 
 input.addEventListener('input', async () => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${input.value}.json?proximity=ip&access_token=${accessToken}&language=pt&limit=4`
@@ -34,6 +53,15 @@ input.addEventListener('input', async () => {
         text.classList.add('suggestion-text')
         text.textContent = place.place_name
         suggestion.appendChild(text)
+
+        suggestion.addEventListener('click', () => {
+            start.addTo(map)
+            end.setLngLat(place.center).addTo(map)
+            map.easeTo({ center: place.center })
+
+            input.value = place.place_name
+            input.classList.remove('focused')
+        })
 
         suggestion.classList.add('suggestion')
         suggestions.appendChild(suggestion)
